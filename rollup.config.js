5db17e0cae8513ead5fbddddc0cc22d28c796309
Haroen Viaene <hello@haroen.me>
2017-08-19 11:24:56 +0200

    chore(build): babili

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,15 @@
 import babel from 'rollup-plugin-babel';
-import uglify from 'rollup-plugin-uglify';
 
 export default {
   entry: 'lib/main.js',
-  dest: 'floating.js',
-  format: 'umd',
-  moduleName: 'floating',
   plugins: [
     babel({
       babelrc: false,
-      presets: ['es2015-rollup']
+      presets: ['es2015-rollup', 'babili'],
     }),
-    uglify()
-  ]
+  ],
+  targets: [
+    { dest: 'build/floating.js', format: 'umd', moduleName: 'floating' },
+    { dest: 'build/floating.es.js', format: 'es' },
+  ],
 };
